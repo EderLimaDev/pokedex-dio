ce6089abf57d8ad1a4a59160622fef4c82611ed1
EderLimaDev <ederlimarh@gmail.com>
2024-01-11 19:17:48 -0300

    adicionado scroll btn e detalhes de css

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,11 +2,13 @@ const pokemonList = document.getElementById("pokemonList");
 const loadMoreBtn = document.getElementById("loadMore");
 const returnBtn = document.querySelector('.return-btn')
 const pokemonDetailsPage = document.querySelector('.pokemonDetails');
+const scrollToTopArrow = document.querySelector('.scrollToTop');
 
 
 const maxRecords = 151;
 const limit = 10;
 let offset = 0;
+let classType = ''
 
 function convertPokemonToLi(pokemon) {
   return `
@@ -31,25 +33,25 @@ function convertPokemonToLi(pokemon) {
 }
 
 
-function loadDetailsPage(id) {
- pokeApi.getOnePokemon(id)
+async function loadDetailsPage(id) {
+ await pokeApi.getOnePokemon(id)
  .then((pokemon) => {
     const {id, name, height, weight} = pokemon
     
     const types = pokemon.types.map((typeSlot) => typeSlot.type.name)
     const photo = pokemon.sprites.other.dream_world.front_default
     const ability = pokemon.abilities.map((abilityMove) => abilityMove.ability.name)
-
+    classType = types[0]
+  
     document.querySelector('.detailNumber').innerHTML = `#${id}`
     document.querySelector('.detailName').innerHTML = name
     
-    document.querySelector('.detail-type1').innerHTML = types[0]
-    document.querySelector('.detail-type2').innerHTML = types[1]
-    
+    document.querySelector('.detail-types').innerHTML = `${types.map((type) => `<li class="type ${type}">${type}</li>`)
+      .join("")}`
     document.querySelector('.detail-photo').src = photo
     document.querySelector('.detail-photo').alt = `pokemon ${name}`
 
-    document.querySelector('.detail-pokemon').classList.add(types[0]) 
+    document.querySelector('.detail-pokemon').classList.add(classType) 
 
     document.querySelector('.pokemon-height').innerHTML = `${height} ft`
     document.querySelector('.pokemon-weight').innerHTML = `${weight} lbs`
@@ -57,7 +59,8 @@ function loadDetailsPage(id) {
     document.querySelector('.pokemon-ability').innerHTML = ability.join(', ')
 
     pokemonDetailsPage.classList.add('open')
-
+    document.querySelector('html').style.overflow='hidden';
+    console.log(classType);
  })
 
 
@@ -66,9 +69,10 @@ function loadDetailsPage(id) {
 
 returnBtn.addEventListener("click", ()=> {
     pokemonDetailsPage.classList.remove('open');
+    document.querySelector('html').style.overflow='auto';
+    document.querySelector('.detail-pokemon').classList.remove(classType)
+
 })
-
-
 
 function loadPokemonItens(offset, limit) {
   pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
@@ -92,3 +96,21 @@ loadMoreBtn.addEventListener("click", () => {
     loadPokemonItens(offset, limit);
   }
 });
+
+
+// Scroll to top arrow function
+
+window.addEventListener("scroll", ()=>  {
+  if ( window.scrollY > 100) {
+    scrollToTopArrow.classList.add('open')
+  } else {
+    scrollToTopArrow.classList.remove('open')
+  }
+})
+
+scrollToTopArrow.addEventListener('click', ()=> {
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+})
